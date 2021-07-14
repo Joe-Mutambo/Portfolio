556462a321a81710f160fff880eca4675f0cc70e
Joe-Mutambo <Snellslyn@gmail.com>
2021-07-15 01:16:23 +0200

    created the contact page and about page fixed

--- app.js
+++ app.js
@@ -8,3 +8,21 @@ for (let i = 0; i < menuLength; i++) {
 		menuItem[i].className = "";
 	}
 }
+
+function focusFunc() {
+	let parent = this.parentNode;
+	parent.classList.add("focus");
+}
+function blurFunc() {
+	let parent = this.parentNode;
+	if (this.value == "") {
+		parent.classList.remove("focus");
+	}
+}
+
+const inputs = document.querySelectorAll(".input");
+
+inputs.forEach((input) => {
+	input.addEventListener("focus", focusFunc);
+	input.addEventListener("blur", blurFunc);
+});
