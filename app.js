21022cf76e9c8cc3bf7c220a67dbd64eb5e533c0
Joe-Mutambo <Snellslyn@gmail.com>
2021-07-18 17:40:55 +0200

    education page developed only projects remaining

--- app.js
+++ app.js
@@ -26,3 +26,31 @@ inputs.forEach((input) => {
 	input.addEventListener("focus", focusFunc);
 	input.addEventListener("blur", blurFunc);
 });
+/*
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n){
+	showSlides(slideIndex = n);
+}
+ function showSlides(n){
+	 var i;
+	 var slides = document.getElementsByClassName("mySlides");
+	 var dots = document.getElementsByClassName("dot");
+	 if(n > slides.length){slideIndex = }
+	 if(n < 1){slideIndex = slides.length}
+	 for(i = 0; i < slides.length; i++){
+		 slides[i].style.display = "none";
+	 }
+
+	 for(i = 0; i < slides.length; i++){
+		 dots[i].className = dots[i].className.replace("active", "")
+	 }
+
+	 showSlides[slideIndex - 1].style.display = "block";
+	 dots[slideIndex - 1].className += "active";
+ }*/
